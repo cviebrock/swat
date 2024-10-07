4e39ff19971ecd1c91f01aeda0c7474c7e03f5ba
Michael Gauthier <mike@silverorange.com>
2024-10-07 16:44:57 -0300

    Use explicit equality check

--- Swat/SwatDateEntry.php
+++ Swat/SwatDateEntry.php
@@ -762,7 +762,7 @@ class SwatDateEntry extends SwatInputControl implements SwatState
             for ($i = $start_day; $i <= $end_day; $i++) {
                 $flydown->addOption($i, $i);
             }
-        } elseif (SwatDate::compare($end_check, $range_end) != -1) {
+        } elseif (SwatDate::compare($end_check, $range_end) !== -1) {
             // extra days at the beginning of the next month allowed
             $days_in_month = $this->valid_range_start->getDaysInMonth();
 
